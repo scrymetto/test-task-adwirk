c29cfea33bb4a1620822765871c6dffa6db2f1ff
hempy <hempy.scrymetto@gmail.com>
2020-03-13 00:16:16 +0300

    add <Lists/>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import {Header} from "./components/header";
+import {Lists} from "./components/lists";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Lists/>
 
     </div>
   );
---
+++ src/components/lists.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Grid} from "@material-ui/core";
+import {Tasks} from "./tasks";
+import {LISTS, MAX_GRID} from "../CONSTS";
+
+export function Lists() {
+    return (
+        <div style={{'margin': '10px'}}>
+            <Grid container justify="center" alignItems='center'>
+                <Grid container direction='row' spacing={2}>
+                    {LISTS.map(list => {
+                        return (
+                            <Grid key={list} item md={MAX_GRID / LISTS.length} sm={MAX_GRID / LISTS.length * 2}
+                                  xs={MAX_GRID}>
+                                <Tasks name={list}/>
+                            </Grid>
+                        )
+                    })}
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
