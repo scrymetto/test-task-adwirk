2cdffdec2f125b88d9e6babd7e2be62e82026daa
hempy <hempy.scrymetto@gmail.com>
2020-03-13 22:01:24 +0300

    <Tasks/> for drop

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -1,33 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { v1 as uuid } from 'uuid';
+import {v1 as uuid} from 'uuid';
 
 import {Grid} from "@material-ui/core";
 import {PaperHeader} from "./paper_header";
 import {NewTask} from "./new_task";
 import {TaskPaper} from "./taskPaper";
+import {useDrop} from "react-dnd";
+import {ItemTypes} from "../CONSTS";
 
 export function Tasks(props) {
     const {list, tasks, setTask} = props;
 
+    const [, drop] = useDrop({
+        accept: ItemTypes.TASK,
+        drop: () => ({key: list.key}),
+    });
+
     const addNewTask = task => {
-        if (task.name){
+        if (task.name) {
             task.id = uuid();
-            setTask({[list.key]:[...tasks, task]})
+            setTask({[list.key]: [...tasks, task]})
         }
     };
 
+    const onMoveTask = (source, taskId, target) => {
+        console.log(source, taskId, target)
+    };
+
     return (
-        <Grid container direction={"column"}>
+        <Grid container direction={"column"} ref={drop}>
             <Grid item>
-                <PaperHeader>{list.name}</PaperHeader>
-                <NewTask addNewTask = {addNewTask}/>
+                <PaperHeader>
+                    {list.name}
+                </PaperHeader>
+                <NewTask addNewTask={addNewTask}/>
             </Grid>
 
             {tasks && tasks.map(task => {
                 return (
                     <Grid key={task.id} item>
-                        <TaskPaper task={task}/>
+                        <TaskPaper task={task} onMove={(id, target) => onMoveTask(list.key, id, target)}/>
                     </Grid>
                 )
             })}
@@ -41,5 +54,4 @@ Tasks.propTypes = {
         name: PropTypes.string.isRequired,
         description: PropTypes.string
     })),
-
 };
